2922cc237bb9f6f3549e02b5940abe38d812f13b
JJJ: JEREMY JAMES JACK (OS) <jeremy@jeremyjamesjack.com>
2026-05-19 10:42:43 -0700

    Create mflg-intake.js

---
+++ js/mflg-intake.js
@@ -0,0 +1,6 @@
+// MFLG Webflow Intake JavaScript
+// External script for MY FAMILY LAW GROUP PLLC intake section.
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("MFLG intake assets loaded.");
+});
